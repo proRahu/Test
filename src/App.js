fe36f14c1d85e8e85e533de9a5ccc452a4ff7ab8
rahul <rahulbirthre@gmail.com>
2023-05-08 15:47:00 +0530

    Test

---
+++ src/App.js
@@ -0,0 +1,16 @@
+//import logo from './logo.svg';
+//import './App.css';
+// import './index.css';
+//import ' ./index.js';
+
+import TodoList from './TodoList';
+function App() {
+  return (
+    <>
+    <nav>ToDo LIST</nav>
+    <TodoList/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/TodoList.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Button from "./components/common/Button"; 
+
+const TodoList = () => {
+    const handleChange=()=>{
+
+    }
+        return(
+            <div>
+                <input type="text" onChange={handleChange} />
+            </div>
+        );
+    }
+
+
+
+
+    export default TodoList;
